15d2971f57d8d9a6230a7f10ab827f9707c24df3
divikumarhari-tech <divikumarhari@gmail.com>
2026-03-10 13:22:14 +0530

    Create If_else program.py

---
+++ If_else program.py
@@ -0,0 +1,93 @@
+#1 finding rectangle or square
+'''
+length=int(input("enter the length"))
+breadth=int(input("enter the breadth"))
+if length==breadth:
+    print("Its a square")
+else:
+    print("Its a rectangle")'''
+#2 square
+
+'''a=int(input("enter a number:"))
+a**=2
+if a<50:
+    print("The square value is less than 50")
+else:
+    print("The sqaure value is greater than 50")'''
+
+#3 subtraction
+'''
+a=int(input("enter number 1:"))
+b=int(input("enter number 2:"))
+sub=a-b
+if sub==0:
+    print("the output is 0")
+else:
+    print("Not 0")'''
+
+#4 Pass or Fail check
+'''
+a=int(input("Enter the mark"))
+if a>=50:
+    print("You passed the exam")
+else:
+    print("You are a failure")'''
+
+#5 divisible by 10
+'''
+a=int(input("Enter a number:"))
+if a%10==0:
+    print("The number is divisible by 10")
+else:
+    print("The number is not divisible by 10")'''
+
+#6 Print largest number
+'''
+a=int(input("Enter a two digit number:"))
+b=a%10
+c=a//10
+if b==c:
+    print("Both are equal")
+elif b>c:
+    print("The biggest digit is",b)
+else:
+    print("The biggest digit is",c)'''
+#7 Value 1
+'''
+a=int(input("Enter a number:"))
+if a==1:
+    print("You can play")
+else:
+    print("You cannot play")'''
+
+#8 Value 1
+'''
+a=int(input("Enter a value:"))
+if a==1:
+    print("Exam will be easy")
+else:
+    print("Exam will be hard")'''
+
+#9 Calculator
+'''
+a=int(input("Enter number 1:"))
+b=int(input("Enter number 1:"))
+c=str(input("Choose an operation +,-,*,/: "))
+if c=="+":
+    print("The addition is:",a+b)
+elif c=="-":
+    print("The subtraction is:",a-b)
+elif c=="*":
+    print("The multiplication is:",a*b)
+elif c=="/":
+    print("The division is:",a/b)
+else:
+    print("Wrong operation")'''
+
+#10 Late fine
+
+a=int(input("
+
+
+    
+
